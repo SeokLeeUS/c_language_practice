7a29b8f3ca57c3b987b2a4dd2a8ae3408d1d81b2
Seok Lee <seokleeuk@gmail.com>
2019-03-08 12:56:59 -0500

    Add files via upload

---
+++ example_49_2_struct_alloc_mem/example_49_2_struct_alloc_mem/example_49_2_struct_alloc_mem.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct _Person {
+	char name[20];
+	int age;
+	char address[100];
+	
+}Person;
+
+int main()
+{
+
+	Person *p1 = malloc(sizeof(Person));
+
+	strcpy(p1->name, "lee");
+	p1->age = 30;
+	strcpy(p1 -> address, "upland view");
+
+	printf("name: %s\n", p1->name);
+	printf("age: %d\n", p1->age);
+	printf("address: %s\n",p1->address);
+
+	free(p1);
+	return 0;
+}
---
+++ example_49_3_pointer_to_struct/example_49_3_pointer_to_struct/example_49_3_pointer_to_struct.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+struct Person {
+	int i;
+	char j;
+};
+
+int main()
+{
+	struct Person p1;
+	struct Person *ptr;
+	ptr = &p1;
+	ptr->i = 30;
+	
+	printf("age: %d\n", p1.i);
+	printf("age: %d\n", ptr->i);
+
+	return 0;
+}
+/*
+구조체 포인터 p1의 멤버 x에 접근하는 방법
+p1->x
+*/
