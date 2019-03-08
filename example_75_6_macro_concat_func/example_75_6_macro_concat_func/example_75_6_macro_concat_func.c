b72f061746337ab9f41d7b18fe2a0059a19cc209
Seok Lee <seokleeuk@gmail.com>
2019-03-08 15:41:58 -0500

    Add files via upload

---
+++ example_75_6_macro_concat_func/example_75_6_macro_concat_func/example_75_6_macro_concat_func.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#define EXECUTER(x) hello##x()
+void hello1()
+{
+	printf("hello 1\n");
+}
+void hello2()
+{
+	printf("hello 2\n");
+}
+
+int main()
+{
+	EXECUTER(1);
+	EXECUTER(2);
+
+	return 0;
+}
+
+/*
+EXECUTER에 1을 넣으면
+hello와 1이 붙게 되므로
+hello1 함수를 호출하고,
+2를 넣으면 hello2 함수를 호출합니다.
+*/
