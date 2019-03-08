0becce158fd88ae7cfb7b72b58ed4132f7c72eaf
Seok Lee <seokleeuk@gmail.com>
2019-03-08 14:42:30 -0500

    Add files via upload

---
+++ example_75_2_macro_function/example_75_2_macro_function/example_75_2_macro_function.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#define PRINT_NUM(x) printf("%d\n",x)
+#define printf
+int main()
+{
+	PRINT_NUM(10);
+	PRINT_NUM(20);
+
+
+	printf("xxxx");
+#undef printf 
+
+	PRINT_NUM(10);
+	PRINT_NUM(20);
+	return 0;
+}
+
+/*
+#define을 사용하면 함수가 아무 동작도 하지 않도록 만들 수 있습니다. 
+다음과 같이 함수 이름으로 된 빈 매크로를 지정하면
+이후에 해당 이름으로 함수를 호출해도 사용할 수 없게 됩니다.
+
+#define printf    // printf를 빈 매크로로 정의
+
+printf("Hello, world!");    // 아무것도 출력되지 않음
+*/
