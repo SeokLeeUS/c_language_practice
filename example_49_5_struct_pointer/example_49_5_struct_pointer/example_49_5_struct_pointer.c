1ea28865e1170acb2956a7c4df26160ab732beee
Seok Lee <seokleeuk@gmail.com>
2019-03-08 13:03:31 -0500

    Add files via upload

---
+++ example_49_5_struct_pointer/example_49_5_struct_pointer/example_49_5_struct_pointer.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Student{
+
+	char name[20];
+	int grade;
+	int class;
+	float average;
+
+};
+
+int main()
+{
+	struct Student *s1 = malloc(sizeof(struct Student)) ;
+
+	strcpy(s1->name,"gildong");
+	s1->grade = 1;
+	s1->class = 3;
+	s1->average = 65.38999f;
+	
+	printf("name: %s\n", s1->name);
+	printf("grade: %d\n", s1->grade);
+	printf("class: %d\n", s1->class);
+	printf("average:%f\n", s1->average);
+
+	free(s1);
+
+	return 0;
+}
