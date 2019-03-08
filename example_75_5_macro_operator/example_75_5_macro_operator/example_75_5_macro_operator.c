c2ae97870f5340fdf20b4996ab29348dadfdd57c
Seok Lee <seokleeuk@gmail.com>
2019-03-08 15:29:36 -0500

    Add files via upload

---
+++ example_75_5_macro_operator/example_75_5_macro_operator/example_75_5_macro_operator.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+#define MUL(a,b) a*b
+#define ADD(a,b) a+b
+
+int main()
+{
+	printf("%d\n", MUL(10, 20));
+	printf("%d\n", MUL(1 + 2, 3 + 4));
+
+	printf("%d\n", ADD(1, 2));
+	printf("%d\n", ADD(1, 2) * 3);
+		
+
+#undef MUL //매크로 해제
+#undef ADD
+
+#define MUL(a,b) ((a)*(b))
+#define ADD(a,b) ((a)+(b))
+
+	printf("%d\n", MUL(10, 20));
+	printf("%d\n", MUL(1 + 2, 3 + 4));
+
+	printf("%d\n", ADD(1, 2));
+	printf("%d\n", ADD(1, 2) * 3);
+
+	return 0;
+}
