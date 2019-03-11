676f3e3720a47bed5a724845279d787ea291aef9
Seok Lee <seokleeuk@gmail.com>
2019-03-11 09:13:42 -0400

    Add files via upload

---
+++ test_60_7_define_function/test_60_7_define_function/test_60_7_define_function.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void printHostname()
+{
+	printf("Jupiter\n");
+}
+void printIPAddress()
+{
+	printf("192.168.10.5\n");
+
+}
+
+
+int main()
+{
+
+	printIPAddress();
+	printHostname();
+	return 0;
+}
