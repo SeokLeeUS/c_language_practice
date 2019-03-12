636fc567dd212552bc68db446b6820ffdd451bb6
Seok Lee <seokleeuk@gmail.com>
2019-03-12 09:10:42 -0400

    Add files via upload

---
+++ example_63_8_string_function_parameter/example_63_8_string_function_parameter/example_63_8_string_function_parameter.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void compareString(char str1, char str2)
+{
+	int int1 = strcmp(str1, str2);
+	if (int1 == 0)
+		printf("equal\n");
+	else
+		printf("not equal\n");
+}
+
+
+int main()
+{
+	char *s1 = malloc(sizeof(char) * 10);
+	strcpy(s1, "Venus");
+	compareString(s1, "Venus");
+	free(s1);
+	
+	return 0;
+}
