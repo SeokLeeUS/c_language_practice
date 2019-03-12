8a99142ab0518dc564752bc1e01eacb1f08df794
Seok Lee <seokleeuk@gmail.com>
2019-03-12 10:28:23 -0400

    Add files via upload

---
+++ example_64_1_func_array_param/example_64_1_func_array_param/example_64_1_func_array_param.c
@@ -0,0 +1,27 @@
+﻿/*
+반환값자료형 함수이름(자료형 매개변수[])
+{
+}
+
+반환값자료형 함수이름(자료형 *매개변수)
+{
+}
+*/
+
+#include <stdio.h>
+void printArray(int arr[], int count)
+{
+	for (int i = 0; i < count; i++)
+	{
+		printf("%d", arr[i]);
+	}
+	printf("\n");
+}
+
+int main()
+{
+	int numArr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	printArray(numArr, sizeof(numArr) / sizeof(int));
+
+	return 0;
+}
