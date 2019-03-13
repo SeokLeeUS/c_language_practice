931c02f898f4689751b4e13f45ec19545277934a
Seok Lee <seokleeuk@gmail.com>
2019-03-13 09:06:47 -0400

    Add files via upload

---
+++ example_67_1_1_recursive_function_call/example_67_1_1_recursive_function_call/example_67_1_1_recursive_function_call.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+void hello(int count)
+{
+
+	if (count == 0)
+		return; // 종료조건을 만듦. count 가 0이면 hello함수를 호출하지 않고 끝냄. 
+
+	printf("hello world %d\n ",count);
+
+	hello(--count);
+}
+
+int main()
+{
+	hello(5);
+	return 0;
+}
+
+
