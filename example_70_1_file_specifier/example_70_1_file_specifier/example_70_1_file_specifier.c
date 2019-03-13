ba3da99b7215f27dffa3410d61ae1bbe8054dd63
Seok Lee <seokleeuk@gmail.com>
2019-03-13 14:56:36 -0400

    Add files via upload

---
+++ example_70_1_file_specifier/example_70_1_file_specifier/example_70_1_file_specifier.c
@@ -0,0 +1,13 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	FILE *fp = fopen("hello.txt", "w");
+	// hello.txt 파일을 쓰기 모드(w)로 열기
+	// 파일 포인터 반환
+	fprintf(fp, "%s %d\n", "Hello", 100);
+	fclose(fp); //파일 포인터 닫기
+	return 0;
+}
+
