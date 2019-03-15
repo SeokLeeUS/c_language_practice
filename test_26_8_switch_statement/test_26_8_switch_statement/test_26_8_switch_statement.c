f3f1e7fd6d95ea8ad83de4449001e502b30ad74d
Seok Lee <seokleeuk@gmail.com>
2019-03-15 14:58:22 -0400

    Add files via upload

---
+++ test_26_8_switch_statement/test_26_8_switch_statement/test_26_8_switch_statement.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	char menu;
+	scanf("%c", &menu);
+	switch (menu)
+	{
+	case 'p':
+		printf("포카리스웨트\n");
+		break;
+	case 'c':
+		printf("콜라\n");
+		break;
+	case 'f':
+		printf("환타\n");
+		break;
+	 default:
+		printf("판매하지 않는 메뉴\n");
+		break;
+
+
+	}
+}
