a4e5cc439787e037123bb279bda6545024cbbd43
Seok Lee <seokleeuk@gmail.com>
2019-03-15 14:57:31 -0400

    Add files via upload

---
+++ float_example_8_9/float_example_8_9/float_example_8_9.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <float.h>
+
+int main()
+{
+	float num1 = FLT_MIN;
+	num1 = num1/1000000000.0f;
+	printf("%e\n", num1);
+	return 0;
+	/*DBL_MIN
+	LDBL_MAX*/
+
+	char c1 = 'a';
+	char c2 = "Hello,world!";
+	unsigned short magic = 'BA';
+
+	printf("%c, %d \n", c1, c2);
+	printf()
+
+}
